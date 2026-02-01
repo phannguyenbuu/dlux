bf598413c05b37b87c13c9ead62557601001d15d
nguyenbuu <phannguyenbuu@gmail.com>
2026-02-01 11:07:40 +0700

    rectpack use

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ def api_scene():
         ("pack_margin_x", "PACK_MARGIN_X"),
         ("pack_margin_y", "PACK_MARGIN_Y"),
         ("pack_bleed", "PACK_BLEED"),
+        ("draw_scale", "DRAW_SCALE"),
         ("pack_grid", "PACK_GRID_STEP"),
         ("pack_angle", "PACK_ANGLE_STEP"),
         ("pack_mode", "PACK_MODE"),
@@ -45,7 +46,7 @@ def api_scene():
         val = request.args.get(key)
         if val is not None:
             os.environ[env_key] = str(val)
-    data = new_toy.compute_scene(new_toy.SVG_PATH, snap)
+    data = new_toy.compute_scene(new_toy.SVG_PATH, snap, render_packed_png=True)
     return jsonify(data)
 
 
@@ -61,6 +62,7 @@ def api_render():
         ("pack_margin_x", "PACK_MARGIN_X"),
         ("pack_margin_y", "PACK_MARGIN_Y"),
         ("pack_bleed", "PACK_BLEED"),
+        ("draw_scale", "DRAW_SCALE"),
         ("pack_grid", "PACK_GRID_STEP"),
         ("pack_angle", "PACK_ANGLE_STEP"),
         ("pack_mode", "PACK_MODE"),
